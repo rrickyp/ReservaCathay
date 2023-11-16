c6b153f4f930309211148ef6fa0a174793df2dd4
rrickyp <rickyrp@connect.hku.hk>
2023-11-16 12:23:44 +0800

    Added the template

---
+++ reserva-cathay/src/layout/AdminLayout/Header/Header.tsx
@@ -0,0 +1,75 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Link from 'next/link'
+import { Button, Container } from 'react-bootstrap'
+import Breadcrumb from '@layout/AdminLayout/Breadcrumb/Breadcrumb'
+import HeaderFeaturedNav from '@layout/AdminLayout/Header/HeaderFeaturedNav'
+import HeaderNotificationNav from '@layout/AdminLayout/Header/HeaderNotificationNav'
+import HeaderProfileNav from '@layout/AdminLayout/Header/HeaderProfileNav'
+import { DatePicker, Select } from 'antd';
+import dayjs from 'dayjs';
+type HeaderProps = {
+  toggleSidebar: () => void;
+  toggleSidebarMd: () => void;
+}
+
+export default function Header(props: HeaderProps) {
+  const { toggleSidebar, toggleSidebarMd } = props
+
+  return (
+    <header className="header sticky-top mb-4 py-2 px-sm-2 border-bottom">
+      <Container fluid className="header-navbar d-flex align-items-center">
+        <Button
+          variant="link"
+          className="header-toggler d-md-none px-md-0 me-md-3 rounded-0 shadow-none"
+          type="button"
+          onClick={toggleSidebar}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </Button>
+        <Button
+          variant="link"
+          className="header-toggler d-none d-md-inline-block px-md-0 me-md-3 rounded-0 shadow-none"
+          type="button"
+          onClick={toggleSidebarMd}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </Button>
+        <Link href="/" className="header-brand d-md-none">
+          <svg width="80" height="46">
+            <title>CoreUI Logo</title>
+            <use xlinkHref="/assets/brand/coreui.svg#full" />
+          </svg>
+        </Link>
+        <div className="header-nav d-none d-md-flex">
+          {/* <HeaderFeaturedNav /> */}
+          <Container fluid>
+        <Breadcrumb />
+      </Container>
+        </div>
+        {/* make it center */}
+        <div className="header-nav ms-auto">
+          <img src="/assets/brand/logo.png" alt="CoreUI Logo" width={35} height={35} />
+        </div>
+        <div className="header-nav ms-auto">
+          <HeaderNotificationNav />
+        </div>
+        <div className="header-nav ms-2">
+          <HeaderProfileNav />
+        </div>
+      </Container>
+      <div className="header-divider border-top my-2 mx-sm-n2" />
+      {/* use datepicker which default is today*/}
+      <DatePicker defaultValue={dayjs()} />
+      {/* create selection */}
+      <Select defaultValue="CX4432" style={{ width: 120, marginLeft: 5}}>
+        <Select.Option value="CX4432">CX4432</Select.Option>
+        <Select.Option value="CX7894">CX7894</Select.Option>
+        <Select.Option value="CX3284">CX3284</Select.Option>
+      </Select>
+
+
+
+    </header>
+  )
+}
---
+++ reserva-cathay/src/layout/AdminLayout/Sidebar/Sidebar.tsx
@@ -0,0 +1,81 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect, useState } from 'react'
+import classNames from 'classnames'
+import { Button } from 'react-bootstrap'
+import SidebarNav from './SidebarNav'
+import HeaderProfileNav from '@layout/AdminLayout/Header/HeaderProfileNav'
+
+export default function Sidebar(props: { isShow: boolean; isShowMd: boolean }) {
+  const { isShow, isShowMd } = props
+  const [isNarrow, setIsNarrow] = useState(false)
+
+  const toggleIsNarrow = () => {
+    const newValue = !isNarrow
+    localStorage.setItem('isNarrow', newValue ? 'true' : 'false')
+    setIsNarrow(newValue)
+  }
+
+  // On first time load only
+  useEffect(() => {
+    if (localStorage.getItem('isNarrow')) {
+      setIsNarrow(localStorage.getItem('isNarrow') === 'true')
+    }
+  }, [setIsNarrow])
+
+  return (
+    <div
+      className={classNames('sidebar d-flex flex-column position-fixed h-100', {
+        'sidebar-narrow': isNarrow,
+        show: isShow,
+        'md-hide': !isShowMd,
+      })}
+      id="sidebar"
+    >
+      {/* add white color to the div */}
+      <div className="d-none d-md-flex align-items-center justify-content-space-between bg-white m-2 pb-1 rounded-2">
+        {/* svg from assets/brand/coreui.svg */}
+        <img
+          src="/assets/brand/coreui.svg"
+          alt="CoreUI Logo"
+          width={305}
+          height={35}
+          className="me-2"
+        />
+          {/* <HeaderProfileNav />
+          <div className="color-black">Agrim Somani</div> */}
+        
+
+      </div>
+
+      <div className="sidebar-nav flex-fill">
+        <SidebarNav />
+      </div>
+
+      <Button
+        variant="link"
+        className="sidebar-toggler d-none d-md-inline-block rounded-0 text-end pe-4 fw-bold shadow-none"
+        onClick={toggleIsNarrow}
+        type="button"
+        aria-label="sidebar toggler"
+      >
+        <FontAwesomeIcon className="sidebar-toggler-chevron" icon={faAngleLeft} fontSize={24} />
+      </Button>
+    </div>
+  )
+}
+
+export const SidebarOverlay = (props: { isShowSidebar: boolean; toggleSidebar: () => void }) => {
+  const { isShowSidebar, toggleSidebar } = props
+
+  return (
+    <div
+      tabIndex={-1}
+      aria-hidden
+      className={classNames('sidebar-overlay position-fixed top-0 bg-dark w-100 h-100 opacity-50', {
+        'd-none': !isShowSidebar,
+      })}
+      onClick={toggleSidebar}
+    />
+  )
+}
